083746765a6cab468690c8042d8dcbf6871dae01
Kavitha Kamal <kavithakamal@gmail.com>
2018-07-31 13:25:48 -0400

    Half way done

--- nestedbrackets.py
+++ nestedbrackets.py
@@ -1,43 +1,40 @@
 import sys
 def check_nested_brackets(filename):
-
-   with open(filename, 'r+') as myfile: 
-       expressions = myfile.read()
-   brackets = {} 
-   brackets_list =[]
-   left_brackets = ['(','[','{','<',]
-   right_brackets = [')',']','}','>','*']
-   for i, c in enumerate(expressions):
-      current_exp=None 
-      print i
-      if c in left_brackets:
-        if (c=='(' and expressions[i+1]=='*'  and i<len(expressions)-1):
-            current_exp = c + expressions[i+1]   
-        else:
-            current_exp = c
+    with open(filename, 'r+') as myfile: 
+       exp_list = myfile.read().split('\n')
+    result =[]
  
-      elif c in right_brackets:      
-        if (c=='*' and expressions[i+1]==')' and expressions[i-1] != '(' and i<len(expressions)-1):  
-            current_exp = c + expressions[i+1]   
-        elif c!='*':
-            current_exp = c
-      if current_exp!=None:
-          
-        if current_exp in brackets:
-            brackets[current_exp] += 1
-        else:
-            brackets[current_exp]  = 1
-   print brackets
-   print expressions    
+    for expressions in exp_list:
+        result.append(check_brackets(expressions))   
+    return result             
+     
 
-  
+def check_brackets( expressions ):
+    print expressions
+    brackets = {'(':')','[':']','{':'}','<':'>','(*':'*)'}
+    brackets_list =[]
+    pos=0
+    for i, c in enumerate(expressions):
+        pos=i+1         
+        if c in brackets:
+            brackets_list.append(c)  
+        elif c in brackets.values():
+            open_bracket= next((k for k, v in brackets.items() if v == c),None)
+            print(c, ' - ', open_bracket)
+            print brackets_list[-1],open_bracket
+            if open_bracket and brackets_list[-1] != open_bracket:
+                return 'NOo '+ str(i)     
+            else:
+                brackets_list.pop()    
+    if len(brackets_list)>0:
+        return 'NO ' + str(pos+1)  
+    return 'YES'    
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
   if len(sys.argv) != 2:
-    print 'usage: ./mimic.py file-to-read'
     sys.exit(1)
-  dict = check_nested_brackets(sys.argv[1])
+print(check_nested_brackets(sys.argv[1]))
 
 if __name__ == '__main__':
   main()
