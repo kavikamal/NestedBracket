396ee73b3bae19e9f36161d40a2855e021102f00
Kavitha Kamal <kavithakamal@gmail.com>
2018-07-31 15:14:56 -0400

    Completed teh assessment

--- nestedbrackets.py
+++ nestedbrackets.py
@@ -2,49 +2,38 @@ import sys
 def check_nested_brackets(filename):
     with open(filename, 'r+') as myfile: 
        exp_list = myfile.read().split('\n')
-    result =[]
+    result = " "
     for exp in exp_list:
-        result.append(check_brackets(exp))   
+        result+=(check_brackets(exp)) + "\n"   
     return result             
      
 def check_brackets( exp ):
-    print exp
     brackets = {'(':')','[':']','{':'}','<':'>','(*':'*)'}
     brackets_list = []
-    flag = False
     i=0
     pos=0
-    while i<len(exp):  
+    while i<len(exp): 
+        pos+=1 
         c=exp[i]
         if c == '(' and exp[i+1]=='*'  and i < len(exp)-1:
            c+=exp[i+1] 
-           pos+=1
-           i+=1
-           flag=True 
-        elif c == '*' and flag and i < len(exp)-1 and exp[i+1]==')' :  
+           i+=1       
+        elif c == '*' and i < len(exp)-1 and exp[i+1]==')' :  
            c+=exp[i+1] 
-           pos+=1
            i+=1
-           flag=False  
-
+     
         if c in brackets:
-            if c != '(*':
-               pos+=1
             brackets_list.append(c)     
         elif c in brackets.values():  
             open_bracket= next((k for k, v in brackets.items() if v == c),None)
             if open_bracket and brackets_list[-1] != open_bracket:
-                return 'NOooo' + str(pos)   
+                return 'NO ' + str(pos)   
             else:
-                brackets_list.pop()  
-        else:
-          pos+=1  
+                brackets_list.pop()      
         i+=1
                      
     # Opening brackets but no closing
     if len(brackets_list)>0:
-        # Find index of last item in brackets_list
-        # Reverse search to find position
         pos=pos+1
         return 'NO ' + str(pos)
     return 'YES'    
@@ -53,7 +42,10 @@ def check_brackets( exp ):
 def main():
   if len(sys.argv) != 2:
     sys.exit(1)
-print(check_nested_brackets(sys.argv[1]))
+result = check_nested_brackets(sys.argv[1])
+file = open("nestedbracketoutput.txt","w") 
+file.write(result)
+file.close()
 
 if __name__ == '__main__':
   main()
